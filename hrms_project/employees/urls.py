0c6b195eeaf0f041298de3d42a55db4b7fcb8cc5
Prashanth Kanekanti <kanakantiprashanth2@gmail.com>
2026-01-11 14:15:26 +0530

    bugs solved in urls

--- hrms_project/employees/urls.py
+++ hrms_project/employees/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns=[
-    path('home/', home),
+    path('', home),
     path('employees/', employee_list),
     path('employees/<int:employee_id>/', employee_detail),
     path('report/', report),
@@ -12,7 +12,7 @@ urlpatterns=[
    #API endpoints
     path('api/employee_data/',add_employee),
     path('api/employee_list/',list_employees),
-    # path('api/employees_list/',EmployeeListView.as_view()),
+    path('api/employees_list/',EmployeeListView.as_view()),
     path('api/mark_attendance/',mark_attendance),
     path('api/emp_attendance/', AttendanceCreateView.as_view()),
     path('api/emp_attendance/<int:employee_id>/',employee_attendance),
